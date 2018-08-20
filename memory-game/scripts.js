634c587e4cde51bddb259145ca34ce24227a59f7
Jack Cooper <JCooper@executivegrapevine.com>
2018-08-20 16:06:21 +0100

    card game javascript refactor

--- memory-game/scripts.js
+++ memory-game/scripts.js
@@ -1,8 +1,65 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    console.log('yuuup');
-    console.log(this);
+    if(lockBoard) return;
+    if(this === firstCard) return;
+    this.classList.add('flip');
+
+    if(!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
+
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
 }
 
-cards.forEach(card => card addEventListener("click", flipCard));
+
+function checkForMatch() {
+    // do cards match?
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+    isMatch ? disableCards() : upflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListenet('click', flipCard);
+    secondCard.removeEventListenet('click', flipCard);
+
+    resetBoard();   
+}
+
+function upflipCards() {
+    lockBoard = true;
+    // not a match
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    },1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]; 
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
